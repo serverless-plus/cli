52fa354d14188d002ec32b61a3d554ad5bd4fe86
yugasun <yuga_sun@163.com>
2020-10-23 19:01:51 +0800

    fix: credentials token

--- src/cli/constants.ts
+++ src/cli/constants.ts
@@ -5,12 +5,12 @@ import { Credential } from '../typings';
 const configPath = join(homedir(), '.slsplus.conf');
 
 const getCredential = (): Credential | null => {
-  const { TENCENT_SECRET_ID, TENCENT_SECRET_KEY, TENCENT_SECRET_TOKEN } = process.env;
+  const { TENCENT_SECRET_ID, TENCENT_SECRET_KEY, TENCENT_TOKEN } = process.env;
   if (TENCENT_SECRET_ID && TENCENT_SECRET_KEY) {
     return {
       secretId: TENCENT_SECRET_ID,
       secretKey: TENCENT_SECRET_KEY,
-      token: TENCENT_SECRET_TOKEN,
+      token: TENCENT_TOKEN,
     };
   }
   return null;
